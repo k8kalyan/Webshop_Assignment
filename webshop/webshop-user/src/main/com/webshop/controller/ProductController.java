7028331432a9c4bda70307447e241081d8c859eb
Kalyan <k8kalyan@gmail.com>
2015-06-29 11:21:51 +0530

    Modified the file

--- webshop/webshop-user/src/main/com/webshop/controller/ProductController.java
+++ webshop/webshop-user/src/main/com/webshop/controller/ProductController.java
@@ -44,11 +44,45 @@ public class ProductController {
 	ProductManager productmanager;
 	@RequestMapping("product.action")
 	public ModelAndView onSubmit(@RequestParam String product,Model model){
-		List<ProductEntity> productlist=productmanager.getproductlist();
-		ModelAndView model1 = new ModelAndView("admin/adminhome");
-		model1.addObject("lists", productlist);
-		model1.addObject("product", product);
-		return model1; 
+		List<ProductEntity> prList=productmanager.getproductlist(product);
+		List<testProduct> testProductList = new ArrayList<testProduct>();	
+				
+		Iterator<ProductEntity> iterator = prList.iterator();
+		while (iterator.hasNext()) {
+			ProductEntity testPr = iterator.next();
+			testProduct a = new testProduct();
+			a.setId(testPr.getId().toString());
+			a.setName(testPr.getName().toString());
+			a.setDesc(testPr.getDesc().toString());
+			a.setPrice(testPr.getPrice().toString());
+			testProductList.add(a);
+		}
+		
+		productList productList = new productList();
+		productList.setProducts(testProductList);
+		
+		 return new ModelAndView("admin/adminhome" , "productList", productList);
 	}
+	
+	List<OrderEntity> orderlist=new ArrayList<OrderEntity>();
+	@RequestMapping(value="addtocart.action", method=RequestMethod.GET )	
+	public String addToCart(@RequestParam("id") String id,@RequestParam("name") String name , @RequestParam("price") String price ,Model model,HttpSession session){
+		
+		OrderEntity orderentity=new OrderEntity();
+		orderentity.setPrice(Double.valueOf(price));
+		orderentity.setProductid(Integer.parseInt(id));
+		orderentity.setQuantity(1);
+		
+		System.out.println("Order entity"+orderentity.getId()+"Price"+orderentity.getPrice());
+		orderlist.add(orderentity);
+		session.setAttribute("cart", orderlist);		
+		return "admin/cart";
+		}
 
+	
+	@RequestMapping(value="removefromcart.action",method=RequestMethod.GET)
+	public String removeFromCart(@RequestParam("id") int id,Model model){
+		orderlist.remove(id);
+		return "admin/cart";
+	}
 }
