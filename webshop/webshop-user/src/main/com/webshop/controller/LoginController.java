21282b58ba24bccb8b887a3813631898267a30e6
Kalyan <k8kalyan@gmail.com>
2015-06-30 16:01:20 +0530

    Modified the file

--- webshop/webshop-user/src/main/com/webshop/controller/LoginController.java
+++ webshop/webshop-user/src/main/com/webshop/controller/LoginController.java
@@ -56,7 +56,7 @@ public class LoginController {
 	 * */
 	@RequestMapping("loginuser.action")
 	public String getAuthenticated(@RequestParam String username,@RequestParam String password,HttpSession session,Model model){
-
+System.out.println("Successandfail");
 		UserEntity user;
 		if(username.equals("") || password.equals("")){
 			return "login";
