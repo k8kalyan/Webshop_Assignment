5d598a3f8a4cb99c26ea4de5e89c5002ce34e1d4
Kalyan <k8kalyan@gmail.com>
2015-06-30 16:04:22 +0530

    Modified the file

--- webshop/webshop-user/src/main/com/webshop/controller/ProductController.java
+++ webshop/webshop-user/src/main/com/webshop/controller/ProductController.java
@@ -1,14 +1,22 @@
 package com.webshop.controller;
-
-
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.webshop.registration.model.OrderEntity;
 import com.webshop.registration.model.ProductEntity;
+import com.webshop.registration.model.ProductList;
+import com.webshop.registration.model.TestProduct;
 import com.webshop.registration.service.ProductManager;
 /**
  * ProductController prepares the order entity object with price, product and quantity information. 
