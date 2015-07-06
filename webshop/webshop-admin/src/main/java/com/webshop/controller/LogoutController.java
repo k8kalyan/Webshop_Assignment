3ba70c806fa0b383ce050ac86a605dd256419760
Kalyan <k8kalyan@gmail.com>
2015-07-06 09:36:19 +0530

    Added thr File

---
+++ webshop/webshop-admin/src/main/java/com/webshop/controller/LogoutController.java
@@ -0,0 +1,44 @@
+package com.webshop.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * Logout controller class  provides implementations for user logged details. 
+ * <P>
+ * <B> Visibility decisions: </B>
+ * <P>
+ * Unless otherwise noted, attributes are private, and a public getter and
+ * setter is provided for each.
+ * <P>
+ * <B> Design/implementation notes: </B>
+ * <P>
+ * Document any decisions, assumptions, issues, or other notes regarding the
+ * implementation of this class.
+ * <P>
+ * <P>
+ * <B> Revision History: </B>
+ * 
+ * <PRE>
+ * 
+ * =============================================================================
+ * Prior Date            By                  Version  Project/CSR  Description 
+ * ---------- --------------------------   ---------- ------------ ------------ 
+ * 18/06/2015         kalyan             N/A          webshop        Created.
+ * 
+ * =============================================================================
+ * 
+ * </PRE>
+ */
+
+@Controller
+public class LogoutController {
+	@RequestMapping("logout.action")
+	public String logout(HttpServletRequest request,HttpSession session){
+		request.getSession().invalidate();
+		return "login";
+	}
+
+}
