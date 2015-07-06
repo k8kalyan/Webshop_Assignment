9ebb6dd100a6fb4e4ed6319676bf2c8492bc9a5a
Kalyan <k8kalyan@gmail.com>
2015-07-06 10:00:11 +0530

    Modified the File

---
+++ webshop/webshop-core/src/main/java/com/webshop/registration/service/AdminManagerImpl.java
@@ -0,0 +1,85 @@
+package com.webshop.registration.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.webshop.registration.model.ProductCategories;
+import com.webshop.registration.model.ProductEntity;
+import com.webshop.registration.repository.AdminDao;
+/**
+ * AdminManagerImpl invoke AdminManager class and calls the methods addCategory,deleteCategory and addproduct. 
+ * <P>
+ * <B> Visibility decisions: </B>
+ * <P>
+ * Unless otherwise noted, attributes are private, and a public getter and
+ * setter is provided for each.
+ * <P>
+ * <B> Design/implementation notes: </B>
+ * <P>
+ * Document any decisions, assumptions, issues, or other notes regarding the
+ * implementation of this class.
+ * <P>
+ * <P>
+ * <B> Revision History: </B>
+ * 
+ * <PRE>
+ * 
+ * =============================================================================
+ * Prior Date            By                  Version  Project/CSR  Description 
+ * ---------- --------------------------   ---------- ------------ ------------ 
+ * 18/06/2015         kalyan             N/A          webshop        Created.
+ * 
+ * =============================================================================
+ * 
+ * </PRE>
+ */
+
+
+@Service
+public class AdminManagerImpl implements AdminManager {
+	
+	@Autowired
+	AdminDao admindao;
+	public List<ProductCategories> getproduct_categorylist() {
+			
+		return admindao.getproduct_categorylist();
+	}
+	 /**
+     * This Method Used to delete the category.
+     * @param id 
+     */
+	public boolean deletecategory(Integer id){
+		return admindao.deletecategory(id);
+	}
+	 /**
+     * This Method Used to add the category.
+     * @param id 
+     */
+	public void addCategory(ProductCategories productCategory)
+	{
+		admindao.addCategory(productCategory);
+	}
+	 /**
+     * This Method Used to add the product.
+     * @param id 
+     */
+	public void addProduct(ProductEntity product)
+	{
+		admindao.addProduct(product);
+	}
+	 /**
+     * This Method Used to get the productlist.
+     * @param ProductEntity 
+     */
+	public List<ProductEntity> getproduct_list() {
+		return admindao.getproduct_list();
+	}
+	 /**
+     * This Method Used to delete the product.
+     * @param id 
+     */
+	public boolean deleteProduct(Integer id){
+		return admindao.deleteProduct(id);
+	}
+
+}
