b773987e553b09b117bc13bf793e2d8ef5c3524c
Kalyan <k8kalyan@gmail.com>
2015-07-06 09:59:47 +0530

    Modified the File

---
+++ webshop/webshop-core/src/main/java/com/webshop/registration/service/AdminManager.java
@@ -0,0 +1,62 @@
+package com.webshop.registration.service;
+
+import java.util.List;
+import com.webshop.registration.model.ProductCategories;
+import com.webshop.registration.model.ProductEntity;
+/**
+ * ProductManager will call the methods addCategory,deleteCategory and addproducts methods. 
+ * <P>
+ * <B> Visibility decisions: </B>
+ * <P>
+ * Unless otherwise noted, attributes are private, and a public getter and
+ * setter is provided for each.
+ * <P>
+ * <B> Design/implementation notes: </B>
+ * <P>
+ * Document any decisions, assumptions, issues, or other notes regarding the
+ * implementation of this class.
+ * <P>
+ * <P>
+ * <B> Revision History: </B>
+ * 
+ * <PRE>
+ * 
+ * =============================================================================
+ * Prior Date            By                  Version  Project/CSR  Description 
+ * ---------- --------------------------   ---------- ------------ ------------ 
+ * 18/06/2015         kalyan             N/A          webshop        Created.
+ * 
+ * =============================================================================
+ * 
+ * </PRE>
+ */
+
+public interface AdminManager {
+	
+	public List<ProductCategories> getproduct_categorylist();
+	 /**
+     * This Method Used to delete the category.
+     * @param id 
+     */
+	boolean deletecategory(Integer id);
+	 /**
+     * This Method Used to add the category.
+     * @param productCategory 
+     */
+	public void addCategory(ProductCategories productCategory);
+	 /**
+     * This Method Used to add the product.
+     * @param product 
+     */
+	public void addProduct(ProductEntity product);
+	 /**
+     * This Method Used to get the productlist.
+     * @param ProductEntity 
+     */
+	public List<ProductEntity> getproduct_list();
+	 /**
+     * This Method Used to delete the product.
+     * @param id 
+     */
+	boolean deleteProduct(Integer id);
+}
