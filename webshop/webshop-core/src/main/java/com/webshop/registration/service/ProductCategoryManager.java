55a7462d39419e85bcb9c296e4a18404ab939fff
Kalyan <k8kalyan@gmail.com>
2015-07-08 10:56:43 +0530

    Modified the File

---
+++ webshop/webshop-core/src/main/java/com/webshop/registration/service/ProductCategoryManager.java
@@ -0,0 +1,38 @@
+package com.webshop.registration.service;
+
+import java.util.List;
+import com.webshop.registration.model.ProductCategories;
+/**
+ * ProductCategoryManager will call the method getProductCategoryList . 
+ * <P>
+ * <B> Visibility decisions: </B>
+ * <P>
+ * Unless otherwise noted, attributes are private, and a public getter and
+ * setter is provided for each.
+ * <P>
+ * <B> Design/implementation notes: </B>
+ * <P>
+ * Document any decisions, assumptions, issues, or other notes regarding the
+ * implementation of this class.
+ * <P>
+ * <P>
+ * <B> Revision History: </B>
+ * 
+ * <PRE>
+ * 
+ * =============================================================================
+ * Prior Date            By                  Version  Project/CSR  Description 
+ * ---------- --------------------------   ---------- ------------ ------------ 
+ * 18/06/2015         kalyan             N/A          webshop        Created.
+ * 
+ * =============================================================================
+ * 
+ * </PRE>
+ */
+
+
+public interface ProductCategoryManager {
+	
+	public List<ProductCategories> getProductCategoriesList();
+
+}
