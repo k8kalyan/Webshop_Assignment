1bbe2f722b26beb76848bb9ceb998dc599a34f78
Kalyan <k8kalyan@gmail.com>
2015-07-10 10:57:33 +0530

    Modified the File

---
+++ webshop/webshop-core/src/main/java/com/webshop/registration/constants/WebshopConstants.java
@@ -0,0 +1,35 @@
+package com.webshop.registration.constants;
+/**
+ * Webshop constant class  provides constant attributes of the project. 
+ * <P>
+ * <B> Visibility decisions: </B>
+ * <P>
+ * Unless otherwise noted, attributes are private, and a public getter and
+ * setter is provided for each.
+ * <P>
+ * <B> Design/implementation notes: </B>
+ * <P>
+ * Document any decisions, assumptions, issues, or other notes regarding the
+ * implementation of this class.
+ * <P>
+ * <P>
+ * <B> Revision History: </B>
+ * 
+ * <PRE>
+ * 
+ * =============================================================================
+ * Prior Date            By                  Version  Project/CSR  Description 
+ * ---------- --------------------------   ---------- ------------ ------------ 
+ * 22/06/2015         kalyan             N/A          webshop        Created.
+ * 
+ * =============================================================================
+ * 
+ * </PRE>
+ */
+
+public class WebshopConstants {
+	
+	public static String ROLE_ADMIN = "AdminUser";
+	public static String ROLE_USER = "NormalUser";
+
+}
