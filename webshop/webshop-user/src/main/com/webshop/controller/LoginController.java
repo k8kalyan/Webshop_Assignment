e36c978d2c0843645658812a90f52422efe86056
Kalyan <k8kalyan@gmail.com>
2015-07-10 11:00:18 +0530

    Modified the File

--- webshop/webshop-user/src/main/com/webshop/controller/LoginController.java
+++ webshop/webshop-user/src/main/com/webshop/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.webshop.controller;
 import java.util.List;
+
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-
 import com.webshop.login.service.LoginManager;
 import com.webshop.registration.model.ProductCategories;
 import com.webshop.registration.model.UserEntity;
 import com.webshop.registration.service.ProductCategoryManager;
+import com.webshop.registration.constants.WebshopConstants;
 /**
  * Login controller class  provides implementations for login user. 
  * <P>
@@ -81,7 +82,7 @@ public class LoginController {
 			session.setAttribute("loggedinUser", user);
 			String authority=loginmanager.getRole(username);
 
-			if(authority.equals("ROLE_USER")){
+			if(authority.equals(WebshopConstants.ROLE_USER)){
 				List<ProductCategories> productCategoriesList=productCategoryManager.getProductCategoriesList();
 
 				return new ModelAndView("admin/adminhome" , "productCategoriesList", productCategoriesList);
