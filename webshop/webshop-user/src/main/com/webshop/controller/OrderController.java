e2207eea41b9f78713f793d9c4bb85dbd93da4cb
Kalyan <k8kalyan@gmail.com>
2015-07-20 10:26:35 +0530

    Modified the File

--- webshop/webshop-user/src/main/com/webshop/controller/OrderController.java
+++ webshop/webshop-user/src/main/com/webshop/controller/OrderController.java
@@ -1,4 +1,11 @@
 package com.webshop.controller;
+import java.util.ArrayList;
+
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -9,6 +16,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import com.webshop.registration.constants.WebshopConstants;
 import com.webshop.registration.model.OrderShipment;
 import com.webshop.registration.service.OrderManager;
+import com.webshop.registration.model.OrderEntity;
 
 
 /**
@@ -48,11 +56,28 @@ public class OrderController {
 
 		return WebshopConstants.USER_ORDER;
 	}
-	@RequestMapping("order.action")
 	
-	public String SubmitForm(@ModelAttribute("orderShipment") OrderShipment orderShipment, BindingResult result,SessionStatus status){
+	@RequestMapping("order.action")
+	public String SubmitForm(@ModelAttribute("orderShipment") OrderShipment orderShipment, BindingResult result,SessionStatus status,HttpSession session){
 
 		manager.addOrder(orderShipment);
+		
+		List<OrderEntity> tempOrderlines = (List<OrderEntity>) session.getAttribute(WebshopConstants.CART);
+					
+	   List<OrderEntity> orderlines=new ArrayList<OrderEntity>();
+	   
+	   for(OrderEntity orderLine : tempOrderlines)
+	   {
+		   OrderEntity tempOrderLine = new OrderEntity();
+		   tempOrderLine.setOrderid(orderShipment.getId());
+		   tempOrderLine.setPrice(orderLine.getPrice());
+		   tempOrderLine.setProductid(orderLine.getProductid());
+		   tempOrderLine.setQuantity(orderLine.getQuantity());
+		   
+		   orderlines.add(tempOrderLine);
+	   }
+		manager.addOrderLines(orderlines);
+		
 		status.setComplete();
 		return WebshopConstants.ORDER_SUCCESS;
 
